cbe8e3269808479eb6805d706ad349b9c9a9cb34
Christine George <christineg593@gmail.com>
2024-07-03 21:10:57 -0400

    updated repository

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-    name='booklover',
-    version='0.1',
-    packages=find_packages(),
-    install_requires=[],
+    name = 'homework09',
+    version = '0.1.0',
+    author = 'Christine George',
+    author_email = 'prz3dt@virginia.edu',
+    packages = ['booklover'],
+    url = 'https://github.com/chr1st1n3g30rg3/homework09',
+    license = 'MIT',
+    description = 'A package for tracking the books Vannessa has read'
 )
-
